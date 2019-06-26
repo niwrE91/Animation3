c4b7b1e2dbd2fb20cc5fc0463227f22c4a5bb268
Erwin Warkentin <niwrE91@dergerat-2.local>
2019-06-26 17:38:16 +0200

    Initial Commit

---
+++ Animation3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Animation3
+//
+//  Created by Erwin Warkentin on 26.06.19.
+//  Copyright © 2019 Erwin Warkentin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
